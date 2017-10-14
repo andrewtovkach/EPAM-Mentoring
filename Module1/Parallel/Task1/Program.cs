6259f3d4fb38406af234234d13f14c9f30e07f03
Andrei_Toukach <Andrei_Toukach@epam.com>
2017-10-14 18:41:09 +0300

    Module 1 - added 1 and 2 parallel tasks

---
+++ Module1/Parallel/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasksList = new List<Task>();
+
+            for (int taskIdx = 0; taskIdx < 100; taskIdx++)
+            {
+                var idx = taskIdx;
+                tasksList.Add(Task.Run(() => TaskOperation(idx)));
+            }
+
+            Task.WaitAll(tasksList.ToArray());
+
+            Console.ReadKey();
+        }
+
+        private static void TaskOperation(int taskIndex)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine($"Task #{taskIndex} – iteration number #{i}");
+            }
+        }
+    }
+}
---
+++ Module1/Parallel/Task2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasksChain = Task.Run(() => GetRandomArray(10))
+                .ContinueWith(task =>
+                {
+                    var resultArray = task.Result;
+                    ShowArray(resultArray);
+                    return GetNewRandomArray(resultArray);
+                })
+                .ContinueWith(task =>
+                {
+                    var resultArray = task.Result;
+                    ShowArray(resultArray);
+                    return GetSortedArray(resultArray);
+                })
+                .ContinueWith(task =>
+                {
+                    var resultArray = task.Result;
+                    ShowArray(resultArray);
+                    return GetAverageValue(resultArray);
+                });
+            Console.WriteLine("Average Value: {0}", tasksChain.Result);
+            Console.ReadKey();
+        }
+
+        private static void ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+        }
+
+        private static int[] GetRandomArray(int length)
+        {
+            var random = new Random();
+            var array = new int[length];
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1000);
+            }
+            return array;
+        }
+
+        private static int[] GetNewRandomArray(int[] array)
+        {
+            var random = new Random();
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] * random.Next(1000);
+            }
+            return array;
+        }
+
+        private static int[] GetSortedArray(int[] array)
+        {
+            Array.Sort(array, (i1, i2) => i2.CompareTo(i1));
+            return array;
+        }
+
+        private static double GetAverageValue(int[] array)
+        {
+            return array.Average();
+        }
+    }
+}
