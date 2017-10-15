16ffdfc2ec904c0ea97d73bb4615c9753aaaf5ab
Andrei_Toukach <Andrei_Toukach@epam.com>
2017-10-15 16:59:07 +0300

    Module 1 - added 3 and 4 parallel tasks

---
+++ Module1/Parallel/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var semaphoreObject = new SemaphoreSlim(2, 2);
+             
+            for (var taskIdx = 0; taskIdx < 10; taskIdx++)
+            {
+                var idx = taskIdx;
+                Task.Factory.StartNew(() =>
+                {
+                    semaphoreObject.Wait();
+                    TaskOperation(idx);
+                    semaphoreObject.Release();
+                });
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void TaskOperation(int taskIndex)
+        {
+            Console.WriteLine("Task index: {0}", taskIndex);
+            Task.Delay(5000).Wait();
+        }
+    }
+}
---
+++ Module1/Parallel/Task4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => TaskOperation())
+                .ContinueWith(task =>
+                {
+                    Console.WriteLine("The continuation task is executing...");
+                    return task;
+                });
+
+            Task.Run(() => TaskOperationWithException())
+               .ContinueWith(task =>
+               {
+                    Console.WriteLine("The parent task has completed with error");
+               }, TaskContinuationOptions.OnlyOnFaulted);
+
+            Task.Run(() => TaskOperationWithException())
+               .ContinueWith(task =>
+                {
+                    Console.WriteLine("The parent task has completed with error");
+                    Console.WriteLine("Parent task thread is reusing...");
+                    task.Wait();
+                }, TaskContinuationOptions.OnlyOnFaulted);
+
+            var cancellationToken = new CancellationToken(true);
+
+            Task.Run(() => TaskOperation(), cancellationToken)
+                .ContinueWith(task =>
+                {
+                    Console.WriteLine("The parent task was cancelled");
+                }, TaskContinuationOptions.OnlyOnCanceled);
+
+            Console.ReadKey();
+        }
+
+        private static void TaskOperation()
+        {
+            Task.Delay(5000).Wait();
+        }
+
+        private static void TaskOperationWithException()
+        {
+            Task.Delay(5000).Wait();
+            throw new Exception();
+        }
+    }
+}
