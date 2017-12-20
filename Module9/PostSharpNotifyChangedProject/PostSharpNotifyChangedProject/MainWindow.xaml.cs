8b5e9f8512168bb84c7a6c8e52e44e30193b765d
Andrei_Toukach <Andrei_Toukach@epam.com>
2017-12-20 10:38:51 +0300

    Module 9 - the third task was completed

--- Module9/PostSharpNotifyChangedProject/PostSharpNotifyChangedProject/MainWindow.xaml.cs
+++ Module9/PostSharpNotifyChangedProject/PostSharpNotifyChangedProject/MainWindow.xaml.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel;
 using System.Windows;
 
 namespace PostSharpNotifyChangedProject
 {
     public partial class MainWindow : Window
     {
-        List<User> items = new List<User>();
+        readonly List<User> _items = new List<User>();
 
         public MainWindow()
         {
             InitializeComponent();
-            items.Add(new User() { Name = "Andrei Toukach", PhoneNumber = "1234567", Mail = "andrei_toukach@epam.com" });
-            items.Add(new User() { Name = "Dzmitry Safarau", PhoneNumber = "7654321", Mail = "dzmitry_safarau@epam.com" });
-            listView.ItemsSource = items;
+            _items.Add(new User { Name = "Andrei Toukach", PhoneNumber = "1234567", Mail = "andrei_toukach@epam.com" });
+            _items.Add(new User { Name = "Dzmitry Safarau", PhoneNumber = "7654321", Mail = "dzmitry_safarau@epam.com" });
+            listView.ItemsSource = _items;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -32,58 +31,13 @@ namespace PostSharpNotifyChangedProject
 
         private void ListView_Selected(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            if (listView.SelectedItems.Count > 0)
-            {
-                User user = (User)listView.SelectedItems[0];
-                name.Text = user.Name;
-                phone.Text = user.PhoneNumber;
-                mail.Text = user.Mail;
-            }
-        }
-    }
+            if (listView.SelectedItems.Count <= 0)
+                return;
 
-    public class User : INotifyPropertyChanged
-    {
-        private string name;
-        private string phoneNumber;
-        private string mail;
-
-        public string Name
-        {
-            get { return name; }
-            set
-            {
-                name = value;
-                OnPropertyChanged("Name");
-            }
-        }
-        public string PhoneNumber
-        {
-            get { return phoneNumber; }
-            set
-            {
-                phoneNumber = value;
-                OnPropertyChanged("PhoneNumber");
-            }
-        }
-
-        public string Mail
-        {
-            get { return mail; }
-            set
-            {
-                mail = value;
-                OnPropertyChanged("Mail");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged(string prop = "")
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
-            }
+            User user = (User)listView.SelectedItems[0];
+            name.Text = user.Name;
+            phone.Text = user.PhoneNumber;
+            mail.Text = user.Mail;
         }
     }
 }
---
+++ Module9/PostSharpNotifyChangedProject/PostSharpNotifyChangedProject/User.cs
@@ -0,0 +1,12 @@
+﻿using PostSharp.Patterns.Model;
+
+namespace PostSharpNotifyChangedProject
+{
+    [NotifyPropertyChanged]
+    public class User
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+    }
+}
